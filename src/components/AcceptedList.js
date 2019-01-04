2a8508755725440cad0f3bc0a11e3de7d7c01e25
billy <billyliadphillips@gmail.com>
2019-01-04 13:04:20 +0000

    Updated whosgoing list

--- src/components/AcceptedList.js
+++ src/components/AcceptedList.js
@@ -1,44 +1,45 @@
-import React, { Component } from 'react'
-import { acceptedInvitationRef } from '../Firebase';
-import { connect } from 'react-redux';
-import { setAcceptedInvite } from '../actions/index';
-
+import React, { Component } from "react";
+import { acceptedInvitationRef } from "../Firebase";
+import { connect } from "react-redux";
+import { setAcceptedInvite } from "../actions/index";
 
 class AcceptedList extends Component {
-    componentDidMount() {
-        acceptedInvitationRef.on('value', snap => {
-            let acceptedInvites = [];
-            snap.forEach(acceptedInvite => {
-                const { email, title } = acceptedInvite.val();
-                acceptedInvites.push({email, title})
-            })
-            console.log('Accepted invis', acceptedInvites);
-            this.props.setAcceptedInvite(acceptedInvites);
-        })
-    }
+  componentDidMount() {
+    acceptedInvitationRef.on("value", snap => {
+      let acceptedInvites = [];
+      snap.forEach(acceptedInvite => {
+        const { email, title } = acceptedInvite.val();
+        acceptedInvites.push({ email, title });
+      });
+      console.log("Accepted invis", acceptedInvites);
+      this.props.setAcceptedInvite(acceptedInvites);
+    });
+  }
   render() {
-      console.log('woah', this.props.acceptedInvites)
+    console.log("woah", this.props.acceptedInvites);
     return (
       <div>
-     {
-         this.props.acceptedInvites.map((acceptedInvite, index) => {
-             const { title, email } = acceptedInvite;
-             return (
-                //  div key={}g
-             )
-
-         })
-     }
+        {this.props.acceptedInvites.map((acceptedInvite, index) => {
+          const { title, email } = acceptedInvite;
+          return (
+            <div key={index}>
+              <strong>{title}</strong>, Invitation accepted by <em>{email}</em>
+            </div>
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => {
-    const { acceptedInvites } = state;
-    return {
-        acceptedInvites
-    }
-}
+  const { acceptedInvites } = state;
+  return {
+    acceptedInvites
+  };
+};
 
-export default connect(mapStateToProps, { setAcceptedInvite })(AcceptedList);
+export default connect(
+  mapStateToProps,
+  { setAcceptedInvite }
+)(AcceptedList);
